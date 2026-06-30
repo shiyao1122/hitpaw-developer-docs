f4dfa14e1e3c90407da51cf25ecbd2dfd5c2d240
XUYANG <174588593+Devin-026@users.noreply.github.com>
2026-06-30 11:13:38 +0800

    chore: mount platform auth sync

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,14 +1,16 @@
 import React, { ReactNode } from 'react';
 import CookieConsent from '@site/src/components/CookieConsent';
+import PlatformAuthSync from '@site/src/components/PlatformAuthSync';
 
 interface RootProps {
   children: ReactNode;
 }
 
-export default function Root({ children }: RootProps): JSX.Element {
+export default function Root({ children }: RootProps): React.ReactElement {
   return (
     <>
       {children}
+      <PlatformAuthSync />
       <CookieConsent />
     </>
   );
